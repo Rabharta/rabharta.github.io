da918113fd969f0dc1e51d239bae7fa30d6d94ee
Ed Galligan <170268+edwardgalligan@users.noreply.github.com>
2024-03-11 17:13:40 +0000

    Update decap-cms-customisations.js

--- themes/rabharta/static/admin/js/decap-cms-customisations.js
+++ themes/rabharta/static/admin/js/decap-cms-customisations.js
@@ -1,116 +1,117 @@
 const windowTitleStyle = `${
-    navigator.platform.includes('Mac')?'macos':'windows'
+  navigator.platform.includes('Mac')?'macos':'windows'
 } ${
-    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : ''
+  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : ''
 }`;
 
 const makePageWrap = (title, contents) => h('div', {}, [
-    h('div', {className: `ostitlebar ${windowTitleStyle}`}, `An Rabharta Glas - Green Left | ${title}`),
-    h('div', {id: 'container'},
-        h('div', {id: 'wrapper'}, contents)
-    )
+  h('div', {className: `ostitlebar ${windowTitleStyle}`}, `An Rabharta Glas - Green Left | ${title}`),
+  h('div', {id: 'container'},
+      h('div', {id: 'wrapper'}, contents)
+  )
 ]);
 
 const makeContentSection = (data, body) => h('section', {className: 'main style1'}, [
-    h('div', {}, h('section', {className: 'main style1'}, [
-        h('header', {className: 'small'}, [
-            h('h1', {}, data.title),
-            h('p', {}, data.subtitle)
-        ]),
-        data.banner ? h('div', {className: 'image filtered', dataPosition: 'center'},
-            h('img', {src: data.banner, alt: data.title})
-        ) : null
-    ])),
-    h('div', {className: 'inner'}, [
-        body,
-        data.actions ? 
-            h('div', {className: 'inner'}, h('ul', {className: 'actions special'},
-                data.actions.map(item => h('li', {},
-                    h('a', {className: 'button next', href: item.link}, item.action)
-                ))
-            )) : null
-    ])
+  h('div', {}, h('section', {className: 'main style1'}, [
+      h('header', {className: 'small'}, [
+          h('h1', {}, data.title),
+          h('p', {}, data.subtitle)
+      ]),
+      data.banner ? h('div', {className: 'image filtered', dataPosition: 'center'},
+          h('img', {src: data.banner, alt: data.title})
+      ) : null
+  ])),
+  h('div', {className: 'inner'}, [
+      body,
+      data.actions ? 
+          h('div', {className: 'inner'}, h('ul', {className: 'actions special'},
+              data.actions.map(item => h('li', {},
+                  h('a', {className: 'button next', href: item.link}, item.action)
+              ))
+          )) : null
+  ])
 ]);
 
 const SinglePagePreview = createClass({
-    render() {
-        const data = this.props.entry.getIn(['data']).toJS();
-        const body = this.props.widgetFor('body');
-
-        const contentSection = makeContentSection(data, body);
-        return makePageWrap(data.title, [
-            contentSection
-        ])
-    }
-});
-const PeoplePagePreview = createClass({
   render() {
       const data = this.props.entry.getIn(['data']).toJS();
       const body = this.props.widgetFor('body');
 
-      const contentSection = makeContentSection(data, body);
       return makePageWrap(data.title, [
-          contentSection
+          makeContentSection(data, body)
       ])
+  }
+});
+const PeoplePagePreview = createClass({
+  render() {
+    const data = this.props.entry.getIn(['data']).toJS();
+    const body = this.props.widgetFor('body');
+
+    return makePageWrap(data.title, [
+        makeContentSection(data, body)
+    ]);
   }
 });
 const BlogPostPreview = createClass({
   render() {
-      const data = this.props.entry.getIn(['data']).toJS();
-      const body = this.props.widgetFor('body');
+    const data = this.props.entry.getIn(['data']).toJS();
+    const body = this.props.widgetFor('body');
+    const publishDate = data.date;
+    console.log({ publishDate, data });
 
-      const contentSection = makeContentSection(data, body);
-      return makePageWrap(data.title, [
-          h('em', {className: 'blogpost-publish-date'}, data.date),
-          contentSection
-      ])
+    const contentSection = makeContentSection(data, body);
+    const date = h('em', {className: 'blogpost-publish-date'}, publishDate || 'date here') || 'no date';
+    return makePageWrap(data.title, [
+        date,
+        contentSection
+    ]);
   }
 });
 const HomepagePreview = createClass({
-    render() {
-        const data = this.props.entry.getIn(['data']).toJS();
-        const body = this.props.widgetFor('body');
+  render() {
+      const data = this.props.entry.getIn(['data']).toJS();
+      const body = this.props.widgetFor('body');
 
-        const bannerSection = h('section', {className: 'banner'}, [
-            h('div', {className: 'image', dataPosition: 'right'},
-                h('img', {src: '/theme/images/banner.jpg'})
-            ),
-            h('div', {className: 'content'}, [
-                h('h1', {style: {whiteSpace: 'pre'}}, data.bannerheading),
-                h('h3', {className: 'tagline'}, data.bannersubheading),
-                data.banneractions ? 
-                    h('ul', {className: 'actions special'},
-                        data.banneractions.map(item => h('li', {},
-                            h('a', {className: 'button next', href: item.link}, item.action)
-                        ))
-                    ) : null
-            ])
-        ]);
-        const homepageContentSection = h('section', {className: 'main style1'}, [
-            h('div', {},
-                h('section', {className: 'main style1'}, [
-                    h('header', {className: 'small'}, [
-                        h('h1', {}, data.heading),
-                        body
-                    ]),
-                    data.image ? h('div', {class: 'image', dataPosition: 'center'},
-                        h('img', {src: data.image, alt: data.heading})
-                    ) : null
-                ])
-            ),
-            data.actions ? 
-                    h('div', {className: 'inner'}, h('ul', {className: 'actions special'},
-                        data.actions.map(item => h('li', {},
-                            h('a', {className: 'button next', href: item.link}, item.action)
-                        ))
-                    )) : null
-            ]);
+      const bannerSection = h('section', {className: 'banner'}, [
+          h('div', {className: 'image', dataPosition: 'right'},
+              h('img', {src: '/theme/images/banner.jpg'})
+          ),
+          h('div', {className: 'content'}, [
+              h('h1', {style: {whiteSpace: 'pre'}}, data.bannerheading),
+              h('h3', {className: 'tagline'}, data.bannersubheading),
+              data.banneractions ? 
+                  h('ul', {className: 'actions special'},
+                      data.banneractions.map(item => h('li', {},
+                          h('a', {className: 'button next', href: item.link}, item.action)
+                      ))
+                  ) : null
+          ])
+      ]);
+      const homepageContentSection = h('section', {className: 'main style1'}, [
+          h('div', {},
+              h('section', {className: 'main style1'}, [
+                  h('header', {className: 'small'}, [
+                      h('h1', {}, data.heading),
+                      body
+                  ]),
+                  data.image ? h('div', {class: 'image', dataPosition: 'center'},
+                      h('img', {src: data.image, alt: data.heading})
+                  ) : null
+              ])
+          ),
+          data.actions ? 
+                  h('div', {className: 'inner'}, h('ul', {className: 'actions special'},
+                      data.actions.map(item => h('li', {},
+                          h('a', {className: 'button next', href: item.link}, item.action)
+                      ))
+                  )) : null
+          ]);
 
-        return makePageWrap(data.title, [
-            bannerSection,
-            homepageContentSection
-        ])
-    }
+      return makePageWrap(data.title, [
+          bannerSection,
+          homepageContentSection
+      ])
+  }
 });
 
 CMS.registerPreviewStyle('/admin/css/preview.css');
